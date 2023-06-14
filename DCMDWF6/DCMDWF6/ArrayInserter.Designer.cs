5056ae94d908a53eb739269d015cea62c34762d6
Maddie Chaotica <Dimadracon3@yandex.ru>
2023-06-15 00:28:20 +0300

    Completed sixth lab work

---
+++ DCMDWF6/DCMDWF6/ArrayInserter.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DCMDWF6
+{
+    partial class ArrayInserter
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArrayInserter));
+            this.inputBox = new System.Windows.Forms.ListBox();
+            this.outputBox = new System.Windows.Forms.ListBox();
+            this.numberGenerator = new System.Windows.Forms.Button();
+            this.nValue = new System.Windows.Forms.TextBox();
+            this.kValue = new System.Windows.Forms.TextBox();
+            this.nMessage = new System.Windows.Forms.Label();
+            this.kMessage = new System.Windows.Forms.Label();
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputBox.FormattingEnabled = true;
+            this.inputBox.ItemHeight = 26;
+            this.inputBox.Location = new System.Drawing.Point(12, 12);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(247, 420);
+            this.inputBox.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputBox.FormattingEnabled = true;
+            this.outputBox.ItemHeight = 26;
+            this.outputBox.Location = new System.Drawing.Point(541, 12);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(247, 420);
+            this.outputBox.TabIndex = 1;
+            // 
+            // numberGenerator
+            // 
+            this.numberGenerator.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberGenerator.Location = new System.Drawing.Point(266, 13);
+            this.numberGenerator.Name = "numberGenerator";
+            this.numberGenerator.Size = new System.Drawing.Size(269, 49);
+            this.numberGenerator.TabIndex = 2;
+            this.numberGenerator.Text = "GENERATE MY NUMBERS!";
+            this.numberGenerator.UseVisualStyleBackColor = true;
+            this.numberGenerator.Click += new System.EventHandler(this.numberGenerator_Click);
+            // 
+            // nValue
+            // 
+            this.nValue.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nValue.Location = new System.Drawing.Point(266, 68);
+            this.nValue.Name = "nValue";
+            this.nValue.Size = new System.Drawing.Size(100, 30);
+            this.nValue.TabIndex = 3;
+            this.nValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nValue_KeyPress);
+            // 
+            // kValue
+            // 
+            this.kValue.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kValue.Location = new System.Drawing.Point(435, 68);
+            this.kValue.Name = "kValue";
+            this.kValue.Size = new System.Drawing.Size(100, 30);
+            this.kValue.TabIndex = 4;
+            this.kValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kValue_KeyPress);
+            // 
+            // nMessage
+            // 
+            this.nMessage.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nMessage.Location = new System.Drawing.Point(266, 105);
+            this.nMessage.Name = "nMessage";
+            this.nMessage.Size = new System.Drawing.Size(132, 75);
+            this.nMessage.TabIndex = 5;
+            this.nMessage.Text = "INPUT THE AMOUNT OF ELEMENTS";
+            this.nMessage.UseMnemonic = false;
+            // 
+            // kMessage
+            // 
+            this.kMessage.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kMessage.Location = new System.Drawing.Point(403, 105);
+            this.kMessage.Name = "kMessage";
+            this.kMessage.Size = new System.Drawing.Size(132, 75);
+            this.kMessage.TabIndex = 6;
+            this.kMessage.Text = "INPUT THE POSITON OF YOUR ZERO";
+            this.kMessage.UseMnemonic = false;
+            // 
+            // zeroButton
+            // 
+            this.zeroButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.zeroButton.Image = ((System.Drawing.Image)(resources.GetObject("zeroButton.Image")));
+            this.zeroButton.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.zeroButton.Location = new System.Drawing.Point(265, 175);
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.Size = new System.Drawing.Size(269, 257);
+            this.zeroButton.TabIndex = 7;
+            this.zeroButton.Text = "INSERT MY ZERO";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            this.zeroButton.Click += new System.EventHandler(this.zeroButton_Click);
+            // 
+            // ArrayInserter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zeroButton);
+            this.Controls.Add(this.kMessage);
+            this.Controls.Add(this.nMessage);
+            this.Controls.Add(this.kValue);
+            this.Controls.Add(this.nValue);
+            this.Controls.Add(this.numberGenerator);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "ArrayInserter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "I WILL INSERT INTO YOUR ARRAY, OWO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox inputBox;
+        private System.Windows.Forms.ListBox outputBox;
+        private System.Windows.Forms.Button numberGenerator;
+        private System.Windows.Forms.TextBox nValue;
+        private System.Windows.Forms.TextBox kValue;
+        private System.Windows.Forms.Label nMessage;
+        private System.Windows.Forms.Label kMessage;
+        private System.Windows.Forms.Button zeroButton;
+    }
+}
+
---
+++ DCMDWF6/DCMDWF6/ArrayInserter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace DCMDWF6
+{
+    public partial class ArrayInserter : Form
+    {
+
+        public ArrayInserter()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// Initialisation of an array that is passed between methods
+        /// </summary>
+        int[] array = new int[0];
+        /// <summary>
+        /// Method that handles the click of the click of the number generator button.
+        /// It generates random numbers and adds to the box on the left.
+        /// The amount of generated numbers is equal to the value user inputted in the box
+        /// </summary>
+        private void numberGenerator_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            try
+            {
+                n = Convert.ToInt32(nValue.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("INPUT THE DAMN VALUE IN THE DAMN BOX");
+            }
+            
+            Random Randy = new Random();
+            Array.Resize(ref array,n+1);
+
+            int maxRandNum = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Randy.Next(-Randy.Next(maxRandNum),Randy.Next(maxRandNum));
+                inputBox.Items.Add("Array[" + (i+1) +"] = " + array[i]);
+            }
+
+        }
+        /// <summary>
+        /// Method that handles inputs into nValue box.
+        /// It allows only digits to be inputed in them
+        /// </summary>
+        private void nValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Method that handles inputs into kValue box.
+        /// It allows only digits to be inputed in them
+        /// </summary>
+        private void kValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Method that handles clicks on zeroButton
+        /// When the button is clicked it fills up the box on the right with elements from the box on the left, but at position k it inserts 0
+        /// </summary>
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            int k = 0;
+            try
+            {
+                k = Convert.ToInt32(kValue.Text);
+                n = Convert.ToInt32(nValue.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("INPUT THE DAMN VALUE IN THE DAMN BOX");
+            }
+            int ind = -1;
+
+            for (int i = 0;i < n; i++)
+            {
+                ind++;
+                if (i == k)
+                {
+                    outputBox.Items.Add("Array[" + (ind + 1) + "] = " + 0);
+                    ind++;
+                }
+                if (i < n) 
+                {
+                    outputBox.Items.Add("Array[" + (ind + 1) + "] = " + array[i]);
+                }
+                
+
+            }
+        }
+    }
+}
